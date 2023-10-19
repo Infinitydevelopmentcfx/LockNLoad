90f5febb9ab293574d91f4f22d554d5e9568095d
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-19 11:33:35 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+games { 'gta5' }
+
+fx_version 'adamant'
+version '1.0'
+client_scripts {
+'c/*'
+}
+
+server_scripts {
+'s/*'
+}
+lua54 'yes'
+shared_scripts {
+  'config.lua',
+    '@ox_lib/init.lua',
+
+}
